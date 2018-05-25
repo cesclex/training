f5e5918ac5c7246eca5bfc243d720644ecafa56a
GabrielaRiv <gabriela260894@gmail.com>
2018-05-25 15:07:36 -0600

    agregar proyecto java

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,20 @@
+package javaapplication1;
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        int numero1 = 10; 
+        int numero2 = 5; 
+ 
+     int suma = numero1+numero2; 
+       System.out.println("la suma es: "+suma);//imprime la suma
+     
+     int resta = numero1-numero2;
+      System.out.println("la resta es: "+resta);//imprime la resta 
+     
+     int multiplicacion = numero1*numero2;
+     System.out.println("la multiplicacion es: "+multiplicacion);//imprime la multiplicacion
+
+     int divicion = numero1/numero2;
+     System.out.println("la division es: "+divicion);//imprime la divicion
+    }
+    
+}
