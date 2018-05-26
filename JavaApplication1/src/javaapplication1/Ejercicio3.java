99297321881cd2dd1dfc9323ed87aedb17dbd086
dvillalobos16 <d_villalobos@outlook.com>
2018-05-25 21:57:55 -0600

    Ejercicio 3 Creado por Diego

---
+++ JavaApplication1/src/javaapplication1/Ejercicio3.java
@@ -0,0 +1,33 @@
+package javaapplication1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class OrdenarNombres{
+    public static Scanner leer = new Scanner(System.in);
+    public static void main (String[] arg){
+        
+        /*Ordenar Nombres*/
+            
+        String[] Nombres = new String[5];
+        
+        for(int i=0; i<5; i++){
+            System.out.println("Nombre: ");
+            Nombres[i]=leer.next();
+        }
+
+        System.out.println("\n Nombres Desordenados");        
+        for (String Nombre : Nombres) {
+            System.out.print(Nombre + " / ");
+        }
+        
+        System.out.println("\n Nombres Ordenados");
+        Arrays.sort(Nombres);
+        for (String Nombre : Nombres) {
+            System.out.print(Nombre + " / ");
+        }
+
+        System.out.println("\n");
+
+    }
+}
